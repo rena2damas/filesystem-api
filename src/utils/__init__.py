4ce24863e99131cfe212486190a68790666fe975
Renato Teixeira <rena2damas@gmail.com>
2022-06-19 21:51:59 +0100

    Convert bytes

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,12 +1,21 @@
 import os
 import pwd
 import subprocess
+import typing
 
 from flask_restful import abort
 from werkzeug.http import HTTP_STATUS_CODES
 
 from src.schemas.serlializers.http import HttpResponseSchema
 from src.settings import oas
+
+
+def convert_bytes(num, suffix="B"):
+    for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
+        if abs(num) < 1024.0:
+            return f"{num:.0f} {unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f} Yi{suffix}"
 
 
 def normpath(path):
