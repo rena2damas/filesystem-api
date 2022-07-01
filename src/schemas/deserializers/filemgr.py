a0d33ce9aa841612a9da7c71f0ba5e37e76f4a55
Renato Teixeira <rena2damas@gmail.com>
2022-07-01 12:16:15 +0100

    Update deserializer actions for file manager

--- src/schemas/deserializers/filemgr.py
+++ src/schemas/deserializers/filemgr.py
@@ -1,21 +1,54 @@
-from marshmallow import fields, Schema, validate
+from marshmallow import fields, Schema
+from marshmallow.validate import OneOf
+
 from src.schemas.serlializers.filemgr import FileMgrStatsSchema
 
 
-class FileMgrRequest(Schema):
+class FileMgrBaseRequest(Schema):
     action = fields.String(
-        validate=validate.OneOf([
-            "read", "create", "delete",
-                                 "rename", "search", "details",
-                                 "copy", "move"])
+        validate=OneOf(
+            ["read", "create", "delete", "rename", "search", "details", "copy", "move"]
+        ),
+        allow_none=False,
     )
     path = fields.String()
-    targetPath = fields.String()
+    data = fields.Nested(FileMgrStatsSchema())
+
+
+class FileMgrReadAction(FileMgrBaseRequest):
+    showHiddenItems = fields.Boolean()
+
+
+class FileMgrCreateAction(FileMgrBaseRequest):
     name = fields.String()
-    names = fields.List(fields.String())
+
+
+class FileMgrRenameAction(FileMgrBaseRequest):
+    name = fields.String()
     newName = fields.String()
-    renameFiles = fields.List(fields.String())
+
+
+class FileMgrDeleteAction(FileMgrBaseRequest):
+    names = fields.List(fields.String())
+
+
+class FileMgrDetailsAction(FileMgrBaseRequest):
+    names = fields.List(fields.String())
+
+
+class FileMgrSearchAction(FileMgrBaseRequest):
     showHiddenItems = fields.Boolean()
     caseSensitive = fields.Boolean()
     searchString = fields.String()
-    data = fields.Nested(FileMgrStatsSchema())
+
+
+class FileMgrCopyAction(FileMgrBaseRequest):
+    names = fields.List(fields.String())
+    targetPath = fields.String()
+    renameFiles = fields.List(fields.String())
+
+
+class FileMgrMoveAction(FileMgrBaseRequest):
+    names = fields.List(fields.String())
+    targetPath = fields.String()
+    renameFiles = fields.List(fields.String())
