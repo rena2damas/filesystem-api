dbb8182310ff64afd02b7941a7a2d040c39b19e4
Renato Teixeira <rena2damas@gmail.com>
2022-07-04 13:19:09 +0100

    Fix schema call

--- src/schemas/serializers/filemgr.py
+++ src/schemas/serializers/filemgr.py
@@ -43,7 +43,7 @@ class DetailsResponseSchema(Schema):
 
 
 def dump_stats(**kwargs):
-    return StatsResponseSchema.dump(**kwargs)
+    return StatsResponseSchema().dump(kwargs)
 
 
 def dump_error(**kwargs):
