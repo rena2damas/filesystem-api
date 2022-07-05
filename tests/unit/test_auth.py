9f5c478be81545c801e4a915a7f6f15201d3be58
Renato Teixeira <rena2damas@gmail.com>
2022-07-05 21:14:12 +0100

    Add test for impersonate decorator

--- tests/unit/test_auth.py
+++ tests/unit/test_auth.py
@@ -1,7 +1,23 @@
+import pytest
+
 from src.services.auth import impersonate
 
 
-def test_impersonate():
+def test_impersonate(mocker):
+
+    mocker.patch("os.setuid")
+    mocker.patch("os.setgid")
+
     @impersonate(username="test")
     def decorated_test():
         pass
+
+    mocker.patch("src.utils.user_uid", side_effect=KeyError)
+    decorated_test()  # KeyError was suppressed
+    mocker.patch("src.utils.user_uid", side_effect=TypeError)
+    decorated_test()  # TypeError was suppressed
+    mocker.patch("src.utils.user_uid", side_effect=PermissionError)
+    decorated_test()  # PermissionError was suppressed
+    mocker.patch("src.utils.user_uid", side_effect=Exception)
+    with pytest.raises(Exception):
+        decorated_test()  # no other exception is suppressed
