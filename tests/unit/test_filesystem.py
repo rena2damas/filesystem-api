248fd7eca4a967e81795f08f34dc45a712631970
Renato Teixeira <rena2damas@gmail.com>
2022-07-06 09:37:11 +0100

    Use boolean asserts

--- tests/unit/test_filesystem.py
+++ tests/unit/test_filesystem.py
@@ -14,36 +14,36 @@ def svc():
 
 
 class TestFilesystemSvc:
-    def test_valid_list_files_returns_valid_data(self, svc, fs):
+    def test_list_files(self, svc, fs):
         fs.create_file("/tmp/file.txt")
         file = next(iter(svc.list_files(path="/tmp/")))
         assert file.name == "file.txt"
 
     def test_list_files_on_missing_file_raises_exception(self, svc):
         with pytest.raises(FileNotFoundError) as ex:
-            assert svc.list_files(path="/tmp/missing.txt")
+            svc.list_files(path="/tmp/missing.txt")
         assert "No such file or directory" in str(ex.value)
 
     def test_list_files_on_restricted_path_raises_exception(self, svc, fs):
         fs.create_file("/tmp/root", st_mode=0o000)
         with pytest.raises(PermissionError) as ex:
-            assert svc.list_files(path="/tmp/root")
+            svc.list_files(path="/tmp/root")
         assert "Permission denied" in str(ex.value)
 
-    def test_valid_stats(self, svc, fs):
+    def test_stats(self, svc, fs):
         fs.create_file("/tmp/file.txt")
         stats = svc.stats(path="/tmp/file.txt")
-        assert stat.S_ISREG(stats.st_mode)
+        assert stat.S_ISREG(stats.st_mode) is True
 
     def test_stats_on_missing_path_raises_exception(self, svc):
         with pytest.raises(FileNotFoundError) as ex:
-            assert svc.stats(path="/tmp/missing.txt")
+            svc.stats(path="/tmp/missing.txt")
         assert "No such file or directory" in str(ex.value)
 
     def test_stats_on_restricted_path_raises_exception(self, svc, fs):
         fs.create_dir("/tmp/root", perm_bits=000)
         with pytest.raises(PermissionError) as ex:
-            assert svc.stats(path="/tmp/root")
+            svc.stats(path="/tmp/root")
         assert "Permission denied" in str(ex.value)
 
     def test_save_file(self, svc, fs, mocker):
@@ -51,12 +51,12 @@ class TestFilesystemSvc:
         mocker.patch.object(file, "save")
         svc.save_file(dst="/tmp", file=file)
         assert file.filename == "file.txt"
-        assert not fs.exists("/tmp/file.txt")
+        assert fs.exists("/tmp/file.txt") is False
 
     def test_make_dir(self, svc, fs):
         fs.create_dir("/tmp/")
         svc.make_dir(path="/tmp/", name="dir")
-        assert fs.exists("/tmp/dir")
+        assert fs.exists("/tmp/dir") is True
 
     def test_make_dir_on_existing_path_throws_exception(self, svc, fs):
         fs.create_dir("/tmp/dir/")
@@ -69,8 +69,8 @@ class TestFilesystemSvc:
         dirpath = "/tmp/dir/"
         fs.create_file(filepath)
         fs.create_dir(dirpath)
-        assert svc.exists_path(filepath)
-        assert svc.exists_path(dirpath)
+        assert svc.exists_path(filepath) is True
+        assert svc.exists_path(dirpath) is True
 
     def test_remove_path(self, svc, fs):
         filepath = "/tmp/file.txt"
@@ -79,8 +79,8 @@ class TestFilesystemSvc:
         fs.create_dir(dirpath)
         svc.remove_path(filepath)
         svc.remove_path(dirpath)
-        assert not svc.exists_path(filepath)
-        assert not svc.exists_path(dirpath)
+        assert svc.exists_path(filepath) is False
+        assert svc.exists_path(dirpath) is False
         with pytest.raises(FileNotFoundError):
             assert svc.stats(path="/tmp/missing.txt")
 
@@ -93,8 +93,8 @@ class TestFilesystemSvc:
         fs.create_dir(dst)
         svc.move_path(src=filepath, dst=dst)
         svc.move_path(src=dirpath, dst=dst)
-        assert svc.exists_path(os.path.join(dst, os.path.basename(filepath)))
-        assert svc.exists_path(os.path.join(dst, os.path.basename(dirpath)))
+        assert svc.exists_path(os.path.join(dst, os.path.basename(filepath))) is True
+        assert svc.exists_path(os.path.join(dst, os.path.basename(dirpath))) is True
 
     def test_move_missing_path_throws_exception(self, svc):
         with pytest.raises(FileNotFoundError):
@@ -107,8 +107,8 @@ class TestFilesystemSvc:
         fs.create_dir(dirpath)
         svc.rename_path(src=filepath, dst="/tmp/from.txt")
         svc.rename_path(src=dirpath, dst="/tmp/to")
-        assert svc.exists_path("/tmp/from.txt")
-        assert svc.exists_path("/tmp/to")
+        assert svc.exists_path("/tmp/from.txt") is True
+        assert svc.exists_path("/tmp/to") is True
 
     def test_rename_missing_path_throws_exception(self, svc):
         with pytest.raises(FileNotFoundError):
@@ -149,5 +149,5 @@ class TestFilesystemSvc:
         fs.create_file(filepath)
         fs.create_dir(dirpath)
         assert svc.isfile(filepath)
-        assert not svc.isfile(dirpath)
-        assert not svc.isfile("/tmp/missing.txt")
+        assert svc.isfile(dirpath) is False
+        assert svc.isfile("/tmp/missing.txt") is False
