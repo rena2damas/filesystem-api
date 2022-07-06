93b563f8f4723e4c2602b2eacbf270c45f8eb194
Renato Teixeira <rena2damas@gmail.com>
2022-07-06 14:21:49 +0100

    Add functional tests for filesystem api

--- tests/functional/test_filesystem.py
+++ tests/functional/test_filesystem.py
@@ -25,7 +25,7 @@ class TestFilesystemGET:
         assert response.json == ["file.txt"]
 
     def test_permission_denied_returns_403(self, client, auth, fs):
-        fs.create_file("/tmp/root", st_mode=0o000)
+        fs.create_dir("/tmp/root", perm_bits=000)
         response = client.get("/tmp/root/", headers=auth)
         data = response.json
         assert response.status_code == 403
@@ -62,191 +62,173 @@ class TestFilesystemGET:
         )
         assert response.headers["Content-Type"] == "application/gzip"
 
-#     def test_unsupported_accept_header_path_returns_400(self, client, auth):
-#         headers = {**auth, "accept": "text/html"}
-#         response = client.get("/filesystem/tmp/", headers=headers)
-#         assert response.status_code == 400
-#         assert response.json == {
-#             "code": 400,
-#             "message": "unsupported 'accept' HTTP header",
-#             "reason": "Bad Request",
-#         }
-#
-#
-# class TestFilesystemPOST:
-#     def test_valid_file_returns_201(self, client, auth, mocker):
-#         mocker.patch("src.utils.shell")
-#         response = client.post(
-#             "/filesystem/tmp/",
-#             headers=auth,
-#             data={"files": (io.BytesIO(b"text"), "file.txt")},
-#             content_type="multipart/form-data",
-#         )
-#         assert response.status_code == 201
-#
-#     def test_path_not_a_directory_returns_400(self, client, auth, mocker):
-#         stderr = "/tmp/file.txt: Not a directory"
-#         err = subprocess.CalledProcessError(cmd="", returncode=1, stderr=stderr)
-#         mocker.patch("src.utils.shell", side_effect=err)
-#         response = client.post(
-#             "/filesystem/tmp/file.txt",
-#             headers=auth,
-#             data={"files": (io.BytesIO(b"text"), "file.txt")},
-#             content_type="multipart/form-data",
-#         )
-#         assert response.status_code == 400
-#
-#     def test_create_existing_file_returns_400(self, client, auth, mocker):
-#         mocker.patch("src.utils.shell", return_value="file.txt")
-#         response = client.post(
-#             "/filesystem/tmp/file.txt",
-#             headers=auth,
-#             data={"files": (io.BytesIO(b"text"), "file.txt")},
-#             content_type="multipart/form-data",
-#         )
-#         assert response.status_code == 400
-#
-#     def test_permission_denied_returns_403(self, client, auth, mocker):
-#         stderr = "/tmp/root/: Permission denied"
-#         err = subprocess.CalledProcessError(cmd="", returncode=1, stderr=stderr)
-#         mocker.patch("src.utils.shell", side_effect=err)
-#         response = client.post(
-#             "/filesystem/tmp/root/",
-#             headers=auth,
-#             data={"files": (io.BytesIO(b"text"), "file.txt")},
-#             content_type="multipart/form-data",
-#         )
-#         assert response.status_code == 403
-#         assert response.json == {
-#             "code": 403,
-#             "message": "permission denied",
-#             "reason": "Forbidden",
-#         }
-#
-#     def test_missing_path_returns_404(self, client, auth, mocker):
-#         stderr = "/tmp/missing/: No such file or directory"
-#         err = subprocess.CalledProcessError(cmd="", returncode=1, stderr=stderr)
-#         mocker.patch("src.utils.shell", side_effect=err)
-#         response = client.post(
-#             "/filesystem/tmp/missing/",
-#             headers=auth,
-#             data={"files": (io.BytesIO(b"text"), "file.txt")},
-#             content_type="multipart/form-data",
-#         )
-#         assert response.status_code == 404
-#         assert response.json == {
-#             "code": 404,
-#             "message": "no such file or directory",
-#             "reason": "Not Found",
-#         }
-#
-#
-# class TestFilesystemPUT:
-#     def test_valid_file_returns_204(self, client, auth, mocker):
-#         mocker.patch("src.utils.shell", return_value="file.txt")
-#         response = client.put(
-#             "/filesystem/tmp/file.txt",
-#             headers=auth,
-#             data={"files": (io.BytesIO(b"text"), "file.txt")},
-#             content_type="multipart/form-data",
-#         )
-#         assert response.status_code == 204
-#
-#     def test_path_not_a_directory_returns_400(self, client, auth, mocker):
-#         stderr = "/tmp/file.txt: Not a directory"
-#         err = subprocess.CalledProcessError(cmd="", returncode=1, stderr=stderr)
-#         mocker.patch("src.utils.shell", side_effect=err)
-#         response = client.put(
-#             "/filesystem/tmp/file.txt",
-#             headers=auth,
-#             data={"files": (io.BytesIO(b"text"), "file.txt")},
-#             content_type="multipart/form-data",
-#         )
-#         assert response.status_code == 400
-#
-#     def test_permission_denied_returns_403(self, client, auth, mocker):
-#         stderr = "/tmp/root/: Permission denied"
-#         err = subprocess.CalledProcessError(cmd="", returncode=1, stderr=stderr)
-#         mocker.patch("src.utils.shell", side_effect=err)
-#         response = client.put(
-#             "/filesystem/tmp/root/",
-#             headers=auth,
-#             data={"files": (io.BytesIO(b"text"), "file.txt")},
-#             content_type="multipart/form-data",
-#         )
-#         assert response.status_code == 403
-#         assert response.json == {
-#             "code": 403,
-#             "message": "permission denied",
-#             "reason": "Forbidden",
-#         }
-#
-#     def test_missing_path_returns_404(self, client, auth, mocker):
-#         stderr = "/tmp/missing/: No such file or directory"
-#         err = subprocess.CalledProcessError(cmd="", returncode=1, stderr=stderr)
-#         mocker.patch("src.utils.shell", side_effect=err)
-#         response = client.put(
-#             "/filesystem/tmp/missing/",
-#             headers=auth,
-#             data={"files": (io.BytesIO(b"text"), "file.txt")},
-#             content_type="multipart/form-data",
-#         )
-#         assert response.status_code == 404
-#         assert response.json == {
-#             "code": 404,
-#             "message": "no such file or directory",
-#             "reason": "Not Found",
-#         }
-#
-#     def test_update_missing_file_returns_404(self, client, auth, mocker):
-#         mocker.patch("src.utils.shell", return_value="")
-#         response = client.put(
-#             "/filesystem/tmp/",
-#             headers=auth,
-#             data={"files": (io.BytesIO(b"text"), "file.txt")},
-#             content_type="multipart/form-data",
-#         )
-#         assert response.status_code == 404
-#         assert response.json == {
-#             "code": 404,
-#             "message": "file does not exist",
-#             "reason": "Not Found",
-#         }
-#
-#
-# class TestFilesystemDELETE:
-#     def test_valid_file_returns_204(self, client, auth, mocker):
-#         mocker.patch("src.utils.shell")
-#         response = client.delete("/filesystem/tmp/file.txt", headers=auth)
-#         assert response.status_code == 204
-#
-#     def test_path_is_a_directory_returns_400(self, client, auth, mocker):
-#         stderr = "/tmp/dir/: is a directory"
-#         err = subprocess.CalledProcessError(cmd="", returncode=1, stderr=stderr)
-#         mocker.patch("src.utils.shell", side_effect=err)
-#         response = client.delete("/filesystem/tmp/dir/", headers=auth)
-#         assert response.status_code == 400
-#
-#     def test_permission_denied_returns_403(self, client, auth, mocker):
-#         stderr = "/tmp/root/: Permission denied"
-#         err = subprocess.CalledProcessError(cmd="", returncode=1, stderr=stderr)
-#         mocker.patch("src.utils.shell", side_effect=err)
-#         response = client.delete("/filesystem/tmp/root/", headers=auth)
-#         assert response.status_code == 403
-#         assert response.json == {
-#             "code": 403,
-#             "message": "permission denied",
-#             "reason": "Forbidden",
-#         }
-#
-#     def test_delete_missing_file_returns_404(self, client, auth, mocker):
-#         stderr = "/tmp/file.txt: No such file or directory"
-#         err = subprocess.CalledProcessError(cmd="", returncode=1, stderr=stderr)
-#         mocker.patch("src.utils.shell", side_effect=err)
-#         response = client.delete("/filesystem/tmp/file.txt", headers=auth)
-#         assert response.status_code == 404
-#         assert response.json == {
-#             "code": 404,
-#             "message": "no such file or directory",
-#             "reason": "Not Found",
-#         }
+    def test_unsupported_accept_header_path_returns_400(self, client, auth):
+        headers = {**auth, "accept": "text/html"}
+        response = client.get("/tmp/", headers=headers)
+        assert response.status_code == 400
+        assert response.json == {
+            "code": 400,
+            "message": "unsupported 'accept' HTTP header",
+            "reason": "Bad Request",
+        }
+
+
+class TestFilesystemPOST:
+    def test_file_returns_201(self, client, auth, fs):
+        fs.create_dir("/tmp")
+        response = client.post(
+            "/tmp/",
+            headers=auth,
+            data={"files": (io.BytesIO(b"text"), "file.txt")},
+            content_type="multipart/form-data",
+        )
+        assert response.status_code == 201
+        assert fs.exists("/tmp/file.txt") is True
+        assert open("/tmp/file.txt", "r").read() == "text"
+
+    def test_missing_path_returns_400(self, client, auth, fs):
+        fs.create_dir("/tmp")
+        response = client.post(
+            "/tmp/missing/",
+            headers=auth,
+            data={"files": (None, "file.txt")},
+            content_type="multipart/form-data",
+        )
+        data = response.json
+        assert response.status_code == 400
+        assert data["code"] == 400
+        assert data["reason"] == "Bad Request"
+        assert "No such file or directory" in data["message"]
+
+    def test_missing_data_returns_400(self, client, auth, fs):
+        fs.create_dir("/tmp")
+        response = client.post(
+            "/tmp/",
+            headers=auth,
+            data={},
+            content_type="multipart/form-data",
+        )
+        assert response.status_code == 400
+        assert response.json == {
+            "code": 400,
+            "message": "missing files",
+            "reason": "Bad Request",
+        }
+
+    def test_create_existing_file_returns_400(self, client, auth, fs):
+        fs.create_file("/tmp/file.txt")
+        response = client.post(
+            "/tmp/",
+            headers=auth,
+            data={"files": (None, "file.txt")},
+            content_type="multipart/form-data",
+        )
+        assert response.status_code == 400
+        assert response.json == {
+            "code": 400,
+            "message": "a file already exists in given path",
+            "reason": "Bad Request",
+        }
+
+    def test_permission_denied_returns_403(self, client, auth, fs):
+        fs.create_dir("/tmp/root", perm_bits=000)
+        response = client.post(
+            "/tmp/root/",
+            headers=auth,
+            data={"files": (io.BytesIO(b"text"), "file.txt")},
+            content_type="multipart/form-data",
+        )
+        data = response.json
+        assert response.status_code == 403
+        assert data["code"] == 403
+        assert data["reason"] == "Forbidden"
+        assert "Permission denied" in data["message"]
+
+
+class TestFilesystemPUT:
+    def test_valid_file_returns_204(self, client, auth, fs):
+        fs.create_file("/tmp/file.txt")
+        response = client.put(
+            "/tmp/",
+            headers=auth,
+            data={"files": (io.BytesIO(b"text"), "file.txt")},
+            content_type="multipart/form-data",
+        )
+        assert response.status_code == 204
+        assert open("/tmp/file.txt", "r").read() == "text"
+
+    def test_missing_file_path_returns_400(self, client, auth, fs):
+        fs.create_dir("/tmp")
+        response = client.put(
+            "/tmp/",
+            headers=auth,
+            data={"files": (None, "file.txt")},
+            content_type="multipart/form-data",
+        )
+        assert response.status_code == 400
+        assert response.json == {
+            "code": 400,
+            "reason": "Bad Request",
+            "message": "a file does not exist in given path",
+        }
+
+    def test_wrong_path_returns_400(self, client, auth, fs):
+        fs.create_dir("/tmp")
+        response = client.put(
+            "/tmp/file.txt",
+            headers=auth,
+            data={"files": (None, "file.txt")},
+            content_type="multipart/form-data",
+        )
+        assert response.status_code == 400
+        assert response.json == {
+            "code": 400,
+            "reason": "Bad Request",
+            "message": "a file does not exist in given path",
+        }
+
+    def test_permission_denied_returns_403(self, client, auth, fs):
+        fs.create_file("/tmp/root.txt", st_mode=0o000)
+        response = client.put(
+            "/tmp/",
+            headers=auth,
+            data={"files": (None, "root.txt")},
+            content_type="multipart/form-data",
+        )
+        data = response.json
+        assert response.status_code == 403
+        assert data["code"] == 403
+        assert data["reason"] == "Forbidden"
+        assert "Permission denied" in data["message"]
+
+
+class TestFilesystemDELETE:
+    def test_valid_file_returns_204(self, client, auth, fs):
+        fs.create_file("/tmp/file.txt")
+        response = client.delete("/tmp/file.txt", headers=auth)
+        assert response.status_code == 204
+        assert fs.exists("/tmp/file.txt") is False
+
+    def test_valid_dir_returns_204(self, client, auth, fs):
+        fs.create_dir("/tmp/dir")
+        response = client.delete("/tmp/dir/", headers=auth)
+        assert response.status_code == 204
+        assert fs.exists("/tmp/dir") is False
+
+    def test_delete_missing_path_returns_404(self, client, auth, fs):
+        fs.create_dir("/tmp")
+        response = client.delete("/tmp/file.txt", headers=auth)
+        data = response.json
+        assert response.status_code == 400
+        assert data["code"] == 400
+        assert data["reason"] == "Bad Request"
+        assert "No such file or directory" in data["message"]
+
+    def test_permission_denied_returns_403(self, client, auth, fs):
+        fs.create_dir("/tmp/root", perm_bits=000)
+        response = client.delete("/tmp/root", headers=auth)
+        data = response.json
+        assert response.status_code == 403
+        assert data["code"] == 403
+        assert data["reason"] == "Forbidden"
+        assert "Permission denied" in data["message"]
