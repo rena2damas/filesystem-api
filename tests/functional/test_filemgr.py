6ba1fcf1b758759686beb4ce4985b9732f8a3f62
Renato Teixeira <rena2damas@gmail.com>
2022-07-06 20:27:36 +0100

    Test create action

--- tests/functional/test_filemgr.py
+++ tests/functional/test_filemgr.py
@@ -17,3 +17,23 @@ class TestFileManagerActions:
         assert data["cwd"]["name"] == "tmp"
         assert data["cwd"]["path"] == "/tmp"
         assert len(data["files"]) == 2
+
+    def test_create_action_returns_200(self, client, fs):
+        fs.create_dir("/tmp")
+        response = client.post(
+            "/file-manager/actions",
+            json={
+                "action": "create",
+                "path": "/tmp",
+                "name": "dir",
+                "data": []
+            }
+        )
+        data = response.json
+        assert response.status_code == 200
+        assert len(data["files"]) == 1
+        assert data["files"][0]["name"] == "dir"
+        assert data["files"][0]["path"] == "/tmp/dir"
+        assert data["files"][0]["isFile"] is False
+        assert data["files"][0]["hasChild"] is False
+        assert fs.exists("/tmp/dir")
