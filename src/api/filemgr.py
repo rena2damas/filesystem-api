6c1aa3df1e845ba76723c6ea09597d293efa3f6a
Renato Teixeira <rena2damas@gmail.com>
2022-07-07 18:36:01 +0100

    Adjust swagger spec response content on download action

--- src/api/filemgr.py
+++ src/api/filemgr.py
@@ -195,11 +195,7 @@ class FileManagerDownload(Resource):
         responses:
             200:
                 content:
-                    application/octet-stream:
-                        schema:
-                            type: string
-                            format: binary
-                    application/gzip:
+                    application/*:
                         schema:
                             type: string
                             format: binary
